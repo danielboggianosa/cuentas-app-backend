6c2865f4568947900d22fe788e84a5ec50052483
Daniel Boggiano <dboggiano@itdata.com.pe>
2021-04-17 00:28:01 -0500

    inicio

---
+++ includes/class-cuentas-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       www.danielboggiano.com
+ * @since      1.0.0
+ *
+ * @package    Cuentas
+ * @subpackage Cuentas/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Cuentas
+ * @subpackage Cuentas/includes
+ * @author     Daniel Boggiano <dev@danielboggiano.com>
+ */
+class Cuentas_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
